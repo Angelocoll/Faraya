ca128b0ff53adc7702ece0b474525211f3093f1c
Angelocoll <angelo.collocolo@yh.nackademin.se>
2025-03-03 15:29:11 +0100

    fixa vercel routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-import FarayaEvent from './Components/Faraya'
-import Admin from './Components/Admin'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import FarayaEvent from './Components/Faraya';
+import Admin from './Components/Admin';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-
   return (
-    <>
-   <Router>
+    <Router basename="/">
       <Routes>
         <Route path="/" element={<FarayaEvent />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
     </Router>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
