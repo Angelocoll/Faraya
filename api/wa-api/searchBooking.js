57b4ab87f3d2097d2d21cd045066af5815141d86
Angelocoll <angelo.collocolo@yh.nackademin.se>
2025-03-25 14:44:13 +0100

    dd

--- api/wa-api/searchBooking.js
+++ api/wa-api/searchBooking.js
@@ -1,4 +1,6 @@
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Tillåt alla ursprung, ändra om du vill specificera ursprung
+
   const { auth_hash, restid, email } = req.query;
 
   const apiUrl = `https://app.waiteraid.com/wa-api/searchBooking?auth_hash=${auth_hash}&restid=${restid}&email=${email}`;
@@ -11,6 +13,7 @@ export default async function handler(req, res) {
     }
 
     const data = await response.json();
+
     return res.status(200).json(data);
   } catch (error) {
     return res.status(500).json({ error: "Internal server error" });
